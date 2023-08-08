8f662fb5333fa48adcd23a0057e72fcab3b21770
Vinayak Baddi <vinayakbaddi@gmail.com>
2023-08-08 13:29:28 +0530

    update resize

--- Profession/AsposeTest/QRCode.cs
+++ Profession/AsposeTest/QRCode.cs
@@ -3,6 +3,7 @@ using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -130,6 +131,43 @@ namespace AsposeTest
             resizedBitmap.Save("output.png", SKEncodedImageFormat.Png, 100);
         }
 
+        static MainSkia()
+        {
+            string imagePath = "path_to_your_image.png";
+            int desiredWidth = 300;
+            int desiredHeight = 400;
+
+            // Load the original image using SkiaSharp
+            using (SKBitmap originalBitmap = SKBitmap.Decode(imagePath))
+            {
+                // Calculate the new dimensions while maintaining aspect ratio
+                float aspectRatio = (float)originalBitmap.Width / originalBitmap.Height;
+                int newWidth = desiredWidth;
+                int newHeight = (int)(newWidth / aspectRatio);
+
+                // Check if the new height exceeds the desired height, adjust if needed
+                if (newHeight > desiredHeight)
+                {
+                    newHeight = desiredHeight;
+                    newWidth = (int)(newHeight * aspectRatio);
+                }
+
+                // Create a new bitmap with the resized dimensions
+                using (SKBitmap resizedBitmap = originalBitmap.Resize(new SKImageInfo(newWidth, newHeight), SKFilterQuality.High))
+                {
+                    // Convert the resized bitmap to a byte array (PNG format)
+                    using (SKImage resizedImage = SKImage.FromBitmap(resizedBitmap))
+                    using (SKPixmap pixmap = resizedImage.PeekPixels())
+                    {
+                        byte[] pngData = pixmap.Encode(SKEncodedImageFormat.Png, 100).ToArray();
+
+                        // Save the byte array as a PNG file
+                        File.WriteAllBytes("resized_image.png", pngData);
+                    }
+                }
+            }
+        }
+
         static void qrcoder()
         {
             string dataToEncode = "Hello, QRCoder QR Code!";
@@ -156,70 +194,70 @@ namespace AsposeTest
 
             }
             static void SkiaQR()
-        {
-            // Replace this string with the data you want to encode in the QR code
-            string dataToEncode = "Hello, SkiaSharp QR Code!";
-
-            // Create a QR code generator instance
-            using (var qrGenerator = new SkiaSharp.QrCode.SkiaSharpQRCodeGenerator())
             {
-                // Set the data to be encoded
-                qrGenerator.Text = dataToEncode;
+                // Replace this string with the data you want to encode in the QR code
+                string dataToEncode = "Hello, SkiaSharp QR Code!";
 
-                // Set the size of the QR code image (width and height in pixels)
-                qrGenerator.Size = 300; // You can adjust the size according to your needs
-
-                // Generate the QR code
-                using (var qrCode = qrGenerator.CreateQRCode())
+                // Create a QR code generator instance
+                using (var qrGenerator = new SkiaSharp.QrCode.SkiaSharpQRCodeGenerator())
                 {
-                    // Convert the QR code to a SkiaSharp bitmap
-                    using (var qrBitmap = qrCode.ToBitmap())
-                    {
-                        // Save the QR code image to a file (PNG format)
-                        using (var image = SKImage.FromBitmap(qrBitmap))
-                        using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
-                        using (var stream = System.IO.File.OpenWrite("QRCode.png"))
-                        {
-                            data.SaveTo(stream);
-                        }
+                    // Set the data to be encoded
+                    qrGenerator.Text = dataToEncode;
 
-                        Console.WriteLine("QR code generated and saved as QRCode.png.");
+                    // Set the size of the QR code image (width and height in pixels)
+                    qrGenerator.Size = 300; // You can adjust the size according to your needs
+
+                    // Generate the QR code
+                    using (var qrCode = qrGenerator.CreateQRCode())
+                    {
+                        // Convert the QR code to a SkiaSharp bitmap
+                        using (var qrBitmap = qrCode.ToBitmap())
+                        {
+                            // Save the QR code image to a file (PNG format)
+                            using (var image = SKImage.FromBitmap(qrBitmap))
+                            using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+                            using (var stream = System.IO.File.OpenWrite("QRCode.png"))
+                            {
+                                data.SaveTo(stream);
+                            }
+
+                            Console.WriteLine("QR code generated and saved as QRCode.png.");
+                        }
                     }
                 }
             }
-        }
-        static void asp()
-        {
-            // Set your Aspose.BarCode license here if you have one
-            // License license = new License();
-            // license.SetLicense("Aspose.BarCode.lic");
+            static void asp()
+            {
+                // Set your Aspose.BarCode license here if you have one
+                // License license = new License();
+                // license.SetLicense("Aspose.BarCode.lic");
 
-            // Create an instance of the BarcodeGenerator class
-            BarcodeGenerator generator = new BarcodeGenerator(EncodeTypes.QR);
+                // Create an instance of the BarcodeGenerator class
+                BarcodeGenerator generator = new BarcodeGenerator(EncodeTypes.QR);
 
-            // Set the barcode value (data)
-            string barcodeValue = "Hello, World!";
-            generator.CodeText = barcodeValue;
+                // Set the barcode value (data)
+                string barcodeValue = "Hello, World!";
+                generator.CodeText = barcodeValue;
 
-            // Set the QR code size in pixels
-            int widthInPixels = 14;
-            int heightInPixels = 14;
-            generator.Parameters.Barcode.XDimension.Millimeters = 0.264583f * widthInPixels;
-            generator.Parameters.Barcode.BarHeight.Millimeters = 0.264583f * heightInPixels;
+                // Set the QR code size in pixels
+                int widthInPixels = 14;
+                int heightInPixels = 14;
+                generator.Parameters.Barcode.XDimension.Millimeters = 0.264583f * widthInPixels;
+                generator.Parameters.Barcode.BarHeight.Millimeters = 0.264583f * heightInPixels;
 
-            // Generate the QR code image
-            Bitmap barcodeImage = generator.GenerateBarCodeImage();
+                // Generate the QR code image
+                Bitmap barcodeImage = generator.GenerateBarCodeImage();
 
-            // Save the generated QR code image to a file (optional)
-            string outputPath = "QRCode.png";
-            barcodeImage.Save(outputPath, System.Drawing.Imaging.ImageFormat.Png);
+                // Save the generated QR code image to a file (optional)
+                string outputPath = "QRCode.png";
+                barcodeImage.Save(outputPath, System.Drawing.Imaging.ImageFormat.Png);
 
-            // Display a message indicating success
-            Console.WriteLine($"QR code with size {widthInPixels}x{heightInPixels} pixels generated and saved as {outputPath}");
+                // Display a message indicating success
+                Console.WriteLine($"QR code with size {widthInPixels}x{heightInPixels} pixels generated and saved as {outputPath}");
 
-            // Pause the console to see the output
-            Console.ReadLine();
+                // Pause the console to see the output
+                Console.ReadLine();
+            }
         }
     }
-}
 
