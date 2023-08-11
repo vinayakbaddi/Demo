4a2f407c72ab035189b500743a472c168a390254
Vinayak Baddi <vinayakbaddi@gmail.com>
2023-08-11 16:22:12 +0530

    Active Directory Testing

---
+++ Profession/ActiveDirTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profession
+{
+    public class ActiveDirTest
+    {
+        static void run()
+        {
+            string domain = "yourdomain.local"; // Replace with your domain name
+            string username = "username"; // Replace with the AD username
+            string password = "password"; // Replace with the AD password
+
+            bool isValid = ValidateCredentials(domain, username, password);
+
+            if (isValid)
+            {
+                Console.WriteLine("Credentials are valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials.");
+            }
+        }
+
+        static bool ValidateCredentials(string domain, string username, string password)
+        {
+            using (PrincipalContext context = new PrincipalContext(ContextType.Domain, domain))
+            {
+                return context.ValidateCredentials(username, password);
+            }
+        }
+    }
+}
