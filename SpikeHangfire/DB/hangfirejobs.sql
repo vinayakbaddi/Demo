cf3715faf03de2032e41801ed65fcf9c2cb4d196
Vinayak Baddi <vinayakbaddi@gmail.com>
2024-01-23 15:35:20 +0530

    datetime

--- SpikeHangfire/DB/hangfirejobs.sql
+++ SpikeHangfire/DB/hangfirejobs.sql
@@ -3,6 +3,23 @@ DECLARE @TotalJobsCount INT;
 
 -- Calculate total duration and total number of jobs
 SELECT 
+    @TotalDurationInSeconds = SUM(DATEDIFF(SECOND, CreatedAt, ExpireAt)),
+    @TotalJobsCount = COUNT(*)
+FROM 
+    Hangfire.Job
+WHERE 
+    ExpireAt IS NOT NULL;
+
+-- Calculate average duration in minutes
+SELECT 
+    ISNULL(@TotalDurationInSeconds / NULLIF(@TotalJobsCount * 60, 0), 0) AS AverageDurationInMinutes;
+
+
+DECLARE @TotalDurationInSeconds INT;
+DECLARE @TotalJobsCount INT;
+
+-- Calculate total duration and total number of jobs
+SELECT 
     @TotalDurationInSeconds = SUM(CAST(Json_Value(State, '$.PerformanceDuration') AS INT)),
     @TotalJobsCount = COUNT(*)
 FROM 
